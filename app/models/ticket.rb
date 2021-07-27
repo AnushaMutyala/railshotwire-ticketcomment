3983a9cb4955e1e1c0fcb585c6f9135080f5a48a
unknown <anusha242316@gmail.com>
2021-07-27 17:34:13 +0530

    first commit

---
+++ app/models/ticket.rb
@@ -0,0 +1,17 @@
+class Ticket < ApplicationRecord
+    has_rich_text :content
+    has_many :comments, dependent: :destroy
+
+    broadcasts
+    #after_create_commit -> {broadcast_append_to self}
+    #after_destroy_commit -> {broadcast_remove_to self}
+    #after_update_commit -> {broadcast_replace_to self}
+    broadcasts
+    #after_create_commit -> {broadcast_append_to tickets_path}
+    #after_destroy_commit -> {broadcast_remove_to tickets_path}
+    #after_update_commit -> {broadcast_replace_to tickets_path}
+
+    
+    
+end
+
